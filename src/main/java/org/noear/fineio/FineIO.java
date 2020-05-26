c2e37d9de1faf9b28f6919b1a866adc449e38b24
noear <noear@live.cn>
2020-05-26 23:51:32 +0800

    0.0.1

--- src/main/java/org/noear/fineio/FineIO.java
+++ src/main/java/org/noear/fineio/FineIO.java
@@ -1,12 +1,11 @@
 package org.noear.fineio;
 
 public final class FineIO {
-    public static <T> NetClient<T> client(Protocol<T> protocol, SessionProcessor<T> processor, String hostname, int port)
-    {
-        return NetClient.nio(protocol,processor,hostname,port);
+    public static <T> NetClient<T> client(Protocol<T> protocol, SessionProcessor<T> processor, String hostname, int port) {
+        return NetClient.nio(protocol, processor, hostname, port);
     }
 
     public static <T> NetServer<T> server(Protocol<T> protocol, SessionProcessor<T> processor) {
-        return NetServer.nio(protocol,processor);
+        return NetServer.nio(protocol, processor);
     }
 }
