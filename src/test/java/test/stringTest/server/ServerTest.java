4162c12b4364463808d2f2aa6f41fd6de4265fc1
noear <noear@live.cn>
2020-05-27 19:08:37 +0800

    0.0.1

--- src/test/java/test/stringTest/server/ServerTest.java
+++ src/test/java/test/stringTest/server/ServerTest.java
@@ -9,16 +9,16 @@ public class ServerTest {
         //定义代理
         //
         MessageHandler<String> handler = (session, message)->{
-                System.out.println("我收到：" + message);
-                Thread.sleep(10);
-                session.write("我收到：" + message);
+                //System.out.println("我收到：" + message);
+                //Thread.sleep(10);
+                session.write(message);
         };
 
         //启动服务
         //
         FineIO.server(new StringProtocol())
                 .bind("localhost", 8888)
-                .handle(handler.pools()) //handler.pools() //线程池模式
+                .handle(handler) //handler.pools() //线程池模式
                 .start(false);
     }
 }
